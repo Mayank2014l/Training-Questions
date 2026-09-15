42efed38e48afad11ab60d7d3cad118df9b94f94
Mayank Pradhan <your.email@example.com>
2026-09-15 11:25:44 +0530

    Add list and slicing questions

---
+++ 10.list_7_elements.py
@@ -0,0 +1,7 @@
+t = []
+
+for i in range(1,6):
+    x = list(map(int, input("Enter element: ").split()))
+    t.append(x[0])
+
+print(t)
---
+++ 9.list.py
@@ -0,0 +1,21 @@
+t1 = [ ]
+print(t1)
+print(type(t1))
+t2=[1,2,3,4]
+t3=[11,22,[101,102,103]]
+t4 =[11,"ram",'t',23.5,111]
+t5="techlive"
+t6=list(t5)
+t7="techlive solution pvt ltd"
+t8=t7.split()
+#t9=input("Enter elements ")
+t10=list(map(int,input("Enter elements ")))
+print(t2)
+print(t3)
+print(t4)
+print(t5)
+print(t6)
+print(t7)
+print(t8)
+#print(t9)
+print(t10)
